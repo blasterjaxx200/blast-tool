a24ff04769e07bb2c76ddf1cd16204529b01672e
blasterjaxx200 <alexissonego272@gmail.com>
2024-09-28 11:22:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ try:
     if sys.platform.startswith("win"):
         os.system("python -m pip install --upgrade pip")
         os.system("python -m pip install -r requirements.txt")
-        os.system("python RedTiger.py")
+        os.system("python main.py")
 
     elif sys.platform.startswith("linux"):
         os.system("python3 -m pip3 install --upgrade pip")
         os.system("python3 -m pip3 install -r requirements.txt")
-        os.system("python3 RedTiger.py")
+        os.system("python3 main.py")
 
 except Exception as e:
     print(e)
