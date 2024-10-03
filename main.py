2c23abbd5fcbc72f618f19ac1c43bd736c9fbb16
blasterjaxx200 <alexissonego272@gmail.com>
2024-10-03 12:01:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,26 +30,17 @@ menu = """
 
                    ╔═══════════════════════════════════════════════════╗
                    ║                                                   ║
-                   ║ [1] soon                      [6] soon            ║
+                   ║ [11] discord server info       [16] soon          ║
                    ║                                                   ║
-                   ║ [2] soon                      [7] soon            ║
+                   ║ [12] execute Builder.bat       [17] soon          ║
                    ║                                                   ║ 
-                   ║ [3] soon                      [8] soon            ║ 
+                   ║ [13] soon                      [18] soon          ║ 
                    ║                                                   ║ 
-                   ║ [4] soon                      [9] soon            ║ 
+                   ║ [14] soon                      [19] soon          ║ 
                    ║                                                   ║ 
-                   ║ [5] soon                      [10] soon           ║ 
+                   ║ [15] soon                      [20] soon          ║ 
                    ╚═══════════════════════════════════════════════════╝
-                                  
-
-                              
-                                  
-                                  
-                                  
-                                  
-                                  
-                                  
-                                  """  
+"""
 menu = fade.fire(menu)
 
 def clear_screen():
@@ -67,29 +58,35 @@ def main():
     choice = input("choice : ")
 
     if choice == "1":
-        os.system("python ./src/tool1.py")
+        os.system("python src/tool1.py")
     elif choice == "2":
-        os.system("python ./src/tool2.py")
+        os.system("python src/tool2.py")
     elif choice == "3":
-        os.system("python ./src/tool3.py")
+        os.system("python src/tool3.py")
     elif choice == "4":
-        os.system("python ./src/tool4.py")
+        os.system("python src/tool4.py")
     elif choice == "5":
-        os.system("python ./src/tool5.py")
+        os.system("python src/tool5.py")
     elif choice == "6":
-        os.system("python ./src/tool6.py")
+        os.system("python src/tool6.py")
     elif choice == "7":
-        os.system("python ./src/tool7.py")
+        os.system("python src/tool7.py")
     elif choice == "8":
-        os.system("python ./src/tool8.py")
+        os.system("python src/tool8.py")
     elif choice == "9":
-        os.system("python ./src/tool9.py")
+        os.system("python src/tool9.py")
     elif choice == "10":
-        os.system("python ./src/tool10.py")
+        os.system("python src/tool10.py")
+    elif choice == "11":
+        os.system("python src/tool11.py")
+    elif choice == "12":
+        # Utilisation du chemin complet ou relatif compatible Windows
+        os.system(r'src\\Builder.bat')  # ou utilise le chemin complet
     else:
         print("Merci de rentrer un nombre valide.")
         time.sleep(1)  # Pause pour afficher l'erreur
         main()  # Réaffiche le menu
 
 main()
+
 
